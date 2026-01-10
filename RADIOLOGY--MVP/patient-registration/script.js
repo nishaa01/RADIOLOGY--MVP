16919a0e4bf2347b8109b502affd3dbde2f1f803
nishaa01 <nishayadav8774@gmail.com>
2026-01-10 23:53:55 +0530

    changed pid in js

--- RADIOLOGY--MVP/patient-registration/script.js
+++ RADIOLOGY--MVP/patient-registration/script.js
@@ -75,24 +75,7 @@ const refphysicianLastNameError = document.getElementById("refPhysicianLastNameE
   return true;
 }
 
-/////////////////     PID VALIDATION      ///////////////////
-function validatePID() {
-  const pidValue = pidInput.value.trim();
-  const pidRegex = /^PID\d{4}$/;
 
-  if (!pidValue) {
-    pidError.textContent = "Patient ID is required";
-    return false;
-  }
-
-  if (!pidRegex.test(pidValue)) {
-    pidError.textContent = "PID must be in format PID1234";
-    return false;
-  }
-
-  pidError.textContent = "";
-  return true;
-}
 ///////////// REFFERING PHYSICIAN NAME VALIDATION     //////////////
 
 function validateRefFirstName() {
